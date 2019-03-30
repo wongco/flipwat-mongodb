dbea8d46c05f779caf72d4ba8891d1ce5c4650a9
wongco <ginson@gmail.com>
2019-03-30 01:53:20 -0700

    add get specific card route

--- models/Card.js
+++ models/Card.js
@@ -21,12 +21,23 @@ class Card {
    * @description - gets list of cards from the database - latest to oldest
    * @property {number} limit - numer of items to limit to
    * @property {number} page - pagination option
-   * @return {Promise <[ { id, text, createdat }, ... ]>}
+   * @return {Promise <{ id, text, createdat }>}
    */
   static async getRandomCard() {
     const result = await db.query(
       'SELECT * FROM cards ORDER BY RANDOM() LIMIT 1'
     );
+    return result.rows[0];
+  }
+
+  /**
+   * @description - gets specific cards from the database
+   * @property {number} id - id number of card
+   * @return {Promise <{ id, text, createdat }>}
+   */
+  static async getCard(id) {
+    console.log(id);
+    const result = await db.query('SELECT * FROM cards WHERE id = $1', [id]);
     return result.rows[0];
   }
 }
--- routes/cards.js
+++ routes/cards.js
@@ -12,20 +12,46 @@ const Card = require('../models/Card');
  * desc: get a list of cards
  */
 router.get('/', async (req, res, next) => {
-  const cards = await Card.getCards({});
-  return res.json({
-    cards
-  });
+  try {
+    const cards = await Card.getCards({});
+    return res.json({
+      cards
+    });
+  } catch (err) {
+    const error = new APIError('could not retreive cards', 500);
+    return next(error);
+  }
 });
 
-/** GET - /cards
- * desc: get a list of cards
+/** GET - /cards/random
+ * desc: get a random card
  */
 router.get('/random', async (req, res, next) => {
-  const card = await Card.getRandomCard();
-  return res.json({
-    card
-  });
+  try {
+    const card = await Card.getRandomCard();
+    return res.json({
+      card
+    });
+  } catch (err) {
+    const error = new APIError('could not retreive card', 500);
+    return next(error);
+  }
+});
+
+/** GET - /cards/:id
+ * desc: get a specific card
+ */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const card = await Card.getCard(id);
+    return res.json({
+      card
+    });
+  } catch (err) {
+    const error = new APIError('could not retrieve card!', 404);
+    return next(error);
+  }
 });
 
 /** POST - /cards/:id
